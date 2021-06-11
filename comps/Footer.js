fd66f7207094b8acd93d4db682e15862f74ba142
eric <eowens@meteoreducation.com>
2021-06-10 22:51:24 -0500

    footer tweaks

--- comps/Footer.js
+++ comps/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="flex-1 text-white  text-sm leading-10  ">
         <img src="/POWERBUTTONLOGO_WHITERGB.png" height="70"></img>
       </div> 
-      <div className="flex-1 text-white  text-sm leading-10">
+      <div className="flex-2 text-white  text-sm leading-10">
       © 2021 Romeo Power, Inc. All Rights Reserved.
       </div>
       <div className="flex-1 text-white  text-sm leading-10">
@@ -16,7 +16,7 @@ const Footer = () => {
       <div className="flex-1 text-white  text-sm leading-10">
       <Link href="/terms">Terms of Service</Link>
       </div>
-      <div className="flex-1 text-white  text-sm leading-10">
+      <div className="flex-2 text-white  text-sm leading-10">
       <Link href="/social_media_guidelines">Social Media Community Guidelines</Link>
       </div>
      
