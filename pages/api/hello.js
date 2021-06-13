a3ae95a40bb1619633393de2de3620a9b5d67af6
eric <eowens@meteoreducation.com>
2021-06-12 20:06:27 -0500

    email

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'eric@owens.fyi', // Change to your recipient
+    from: 'eric@owens.fyi', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+    
+    res.status(200).json({ name: process.env.SENDGRID_API_KEY })
+
 }
