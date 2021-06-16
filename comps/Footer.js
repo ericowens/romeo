825284f780d40c6f70ef8e24a049b0178e54f042
eric <eowens@meteoreducation.com>
2021-06-16 13:50:49 -0500

    tweaks post launch 10

--- comps/Footer.js
+++ comps/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
        
 
       <section id="footer" className=" flex justify-center bg-darkaccent ">
-       <div className="flex flex-col md:flex-row max-w-screen-lg  px-4 gap-0 md: gap-4 flex-1 ">
+       <div className="flex flex-col md:flex-row max-w-screen-lg pb-4  md:pb-0  px-4 gap-0 md: gap-4 flex-1 ">
         <div className="flex-1 text-white  text-xs lg:leading-10 p-2  ">
           <img className="" src="/logos/footer.svg" ></img>
         </div> 
