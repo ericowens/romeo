24bcac5fd7d15aeb33e347ea8803561f5aace1ff
eric <eowens@meteoreducation.com>
2021-08-09 13:53:17 -0500

    pdf

---
+++ pages/supplier-terms-conditions.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Navbar from '../comps/Navbar'
+import Image from 'next/image'
+import Link from 'next/link'
+import Footer from '../comps/Footer'
+import Banner from '../comps/Banner'
+import observe_nav from '../comps/observe_nav'
+import React, { useState, useEffect } from 'react'
+
+
+export default function Home() {
+  
+  return (
+    <div>
+      <Head>
+        <title>Romeo</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta httpEquiv = "refresh" content = "0; url = /F-840.003_REV_2_ PUR_TermsConditions(10.28.2017).pdf" />
+        
+      </Head>
+
+      
+    </div>
+  )
+}
