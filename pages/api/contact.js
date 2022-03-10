14b28914f8c479bc341efc5244fa3073103939a1
eric <eowens@meteoreducation.com>
2022-03-10 14:40:45 -0600

    1

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -59,7 +59,7 @@ export default function handler(req, res) {
     const msg = {
       to: mailto, // Change to your recipient
       from: 'eric@owens.fyi', // Change to your verified sender
-      subject: 'Contact Test',
+      subject: 'Contact',
       // text: `Name:${req.body.fname} ${req.body.lname}`,
       html: `Name:${req.body.fname} ${req.body.lname}
       <br/>Email:${req.body.email}
