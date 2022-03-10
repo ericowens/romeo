c406653b762bb20384b3b09a08871d5790867e0b
eric <eowens@meteoreducation.com>
2022-03-10 14:05:53 -0600

    1

--- comps/Slider.js
+++ comps/Slider.js
@@ -12,8 +12,15 @@ const Slider = () => {
     console.log(navs)
   }
 
-  function unmute(src,e) {
-    document.getElementById("slider").muted = false
+  function unmute(e) {
+     let vid = document.getElementById("slider")
+     if(vid.muted){
+      e.target.innerText = 'Mute'
+     }else {
+      e.target.innerText = 'Unmute'
+     }
+      vid.muted = !vid.muted
+    
   }
   
     return (
@@ -28,14 +35,14 @@ const Slider = () => {
           <button  
           onClick={(e) => unmute(e)} 
           className="bg-accent  float-left py-4 px-14 my-2 text-white">
-            Sound (Icon?)
+            Unmute
             </button> 
         </div>
 
 
         <div className="navigation">
           <div  onClick={(e) => vidurl("/vid/yt/Production.mp4",e)}  className="btn active"></div>
-          <div  onClick={(e) => vidurl("/vid/yt/Brighter.mp4",e)} className="btn"></div>
+          <div  onClick={(e) => vidurl("/vid/yt/Brand-720p.mp4",e)} className="btn"></div>
         </div>
 
 
