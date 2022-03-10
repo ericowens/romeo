0b2637b0f50dfe95d577dcc49b66b357dec209ef
eric <eowens@meteoreducation.com>
2022-03-10 12:57:24 -0600

    1

--- comps/Slider.js
+++ comps/Slider.js
@@ -17,13 +17,13 @@ const Slider = () => {
       <section id="hero" className="hero-vid" >
 
         <video id="slider" autoPlay muted loop>
-          <source src="/vid/FINAL ROMEO POWER BF MASTER 360.mp4" type='video/mp4'></source>
+          <source src="/vid/yt/Production.mp4" type='video/mp4'></source>
         </video>
 
 
         <div className="navigation">
-          <div  onClick={(e) => vidurl("/vid/FINAL ROMEO POWER BF MASTER 360.mp4",e)}  className="btn active"></div>
-          <div  onClick={(e) => vidurl("/vid/2.mp4",e)} className="btn"></div>
+          <div  onClick={(e) => vidurl("/vid/yt/Production.mp4",e)}  className="btn active"></div>
+          <div  onClick={(e) => vidurl("/vid/yt/Brighter.mp4",e)} className="btn"></div>
         </div>
 
 
